33136d700d7309869cb29bf08276daa95490f993
Xwen6 <2502102811@qq.com>
2021-06-24 15:19:37 +0800

    v1

--- src/main/java/wyu/xwen/workbench/service/impl/ActivityServiceImpl.java
+++ src/main/java/wyu/xwen/workbench/service/impl/ActivityServiceImpl.java
@@ -1,8 +1,15 @@
 package wyu.xwen.workbench.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import wyu.xwen.workbench.dao.ActivityDao;
 import wyu.xwen.workbench.service.ActivityService;
 
 @Service
 public class ActivityServiceImpl implements ActivityService {
+
+    @Autowired
+    private ActivityDao activityDao;
+
+
 }
